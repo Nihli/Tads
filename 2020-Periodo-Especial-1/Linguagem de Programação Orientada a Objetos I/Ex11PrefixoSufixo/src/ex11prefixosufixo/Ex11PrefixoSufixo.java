8aa6b0795217984885945878701785a5c3b10c6f
Nihli <lia.mellon17@gmail.com>
2020-08-15 19:55:08 -0300

    Adiciona Ex 11 da lista 02

--- 2020-Periodo-Especial-1/Linguagem de Programação Orientada a Objetos I/Ex11PrefixoSufixo/src/ex11prefixosufixo/Ex11PrefixoSufixo.java
+++ 2020-Periodo-Especial-1/Linguagem de Programação Orientada a Objetos I/Ex11PrefixoSufixo/src/ex11prefixosufixo/Ex11PrefixoSufixo.java
@@ -18,36 +18,27 @@ public class Ex11PrefixoSufixo {
      */
     public static void main(String[] args) {
         String[] palavras = new String[10];
-        String[] prefixos = null;
-        String[] sufixos = null;       
-        
+
         for(int i=0;i<palavras.length;i++) {
             System.out.println("Entre com uma palavra:");
             Scanner sc = new Scanner(System.in);
             palavras[i]= sc.next();
         }
+
         for (int i = 0; i < palavras.length; i++) {
-            String prefix = palavras[i].substring(0,1);
-            String sufix = palavras[i].substring(palavras[i].length()-1);
-                for(int j = i+1; j < palavras.length; j++) {
-                        if(palavras[j].startsWith(prefix)){
-//                            prefixos[j]=palavras[j];
-                            System.out.println("Prefixo " + palavras[j]);
-                        }
-                        if(palavras[j].endsWith(sufix)){
-//                            sufixos[j]=palavras[j];
-                            System.out.println("Sufixo " + palavras[j]);
-                        }
-                }    
+            for(int j=0;j<palavras[i].length();j++){
+                String prefix = palavras[i].substring(0, palavras[i].length()-j);
+                String sufix = palavras[i].substring(j);
+                for(int k = i; k < palavras.length; k++) {
+                    if(palavras[k].startsWith(prefix)){
+                        System.out.println("Prefixo\t\t"+ prefix + "\t\t" + palavras[k]);
+                    }
+                    if(palavras[k].endsWith(sufix)){
+                        System.out.println("Sufixo\t\t"+ prefix + "\t\t" + palavras[k]);
+                    }
+                }
+            }
         }
-//        System.out.println("Prefixos:");
-//        for(int i = 0; i < prefixos.length; i++) {
-//            System.out.println(prefixos[i]);
-//        }
-//        System.out.println("Sufixos");
-//        for(int i = 0; i < sufixos.length; i++) {
-//            System.out.println(sufixos[i]);
-//        }
         
     }
     
